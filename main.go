fab3eb9dec0dbd918ba51e48c65cde7173f91b0d
enokawa <enokawa00@gmail.com>
2020-10-30 08:12:23 +0900

    Add listObjectsV2 method

--- main.go
+++ main.go
@@ -9,13 +9,8 @@ import (
 	"os"
 )
 
-func main() {
+func listObjectsV2(input *s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error) {
 	svc := s3.New(session.New())
-	input := &s3.ListObjectsV2Input{
-		Bucket:  aws.String(os.Getenv("S3_BUCKET")),
-		MaxKeys: aws.Int64(5),
-	}
-
 	result, err := svc.ListObjectsV2(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
@@ -28,6 +23,20 @@ func main() {
 		} else {
 			fmt.Println(err.Error())
 		}
+	}
+
+	return result, nil
+}
+
+func main() {
+	input := &s3.ListObjectsV2Input{
+		Bucket:  aws.String(os.Getenv("S3_BUCKET")),
+		MaxKeys: aws.Int64(5),
+	}
+
+	result, err := listObjectsV2(input)
+	if err != nil {
+		fmt.Println(err)
 	}
 
 	for _, v := range result.Contents {

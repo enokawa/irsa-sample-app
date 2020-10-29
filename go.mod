e53645835dd53eabd8a740fa30683f25b8ba0dc4
enokawa <enokawa00@gmail.com>
2020-10-29 23:12:46 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/enokawa/irsa-sample-app
+
+go 1.14
+
+require github.com/aws/aws-sdk-go v1.35.17
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"os"
+)
+
+func main() {
+	svc := s3.New(session.New())
+	input := &s3.ListObjectsV2Input{
+		Bucket:  aws.String(os.Getenv("S3_BUCKET")),
+		MaxKeys: aws.Int64(5),
+	}
+
+	result, err := svc.ListObjectsV2(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeNoSuchBucket:
+				fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+	}
+
+	for _, v := range result.Contents {
+		fmt.Println(*v.Key)
+	}
+}
